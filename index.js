86422abe6461b19ed35d23aa81d698cbae2b461a
Jaden Durnford <jadendurnford@gmail.com>
2022-10-26 17:52:57 -0400

    fixing env variables and cleaning up register function

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const {
 } = require("discord.js");
 require("dotenv").config();
 const redis = require("redis");
-const redisClient = redis.createClient();
+const redisClient = redis.createClient({ url: process.env.REDIS_URL });
 
 redisClient.connect();
 
