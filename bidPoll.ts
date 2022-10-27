6230614934891ee1bb87dbeb782f323db0e30e7d
Jaden Durnford <jadendurnford@gmail.com>
2022-10-27 19:52:44 -0400

    cleaning up api calls with node sdk calls and implementing type checking for floor and bid poll functions

--- bidPoll.ts
+++ bidPoll.ts
@@ -1,58 +1,68 @@
-import axios from "axios";
 import Redis from "ioredis";
 import { TextChannel, EmbedBuilder } from "discord.js";
 const redis = new Redis();
+import { paths } from "@reservoir0x/reservoir-kit-client";
 import logger from "./logger";
+const sdk = require("api")("@reservoirprotocol/v1.0#6e6s1kl9rh5zqg");
 
 export async function bidPoll(channel: TextChannel, contractAddress: string) {
   await redis.connect();
 
-  const {
-    data: {
-      events: [bidData],
-    },
-  } = await axios.get(
-    `https://api.reservoir.tools/events/collections/top-bid/v1?collection=${contractAddress}&sortDirection=desc&limit=1`
-  );
+  const topBidRes = await sdk.getEventsCollectionsTopbidV1({
+    collection: contractAddress,
+    sortDirection: "desc",
+    limit: "1",
+    accept: "*/*",
+  });
+  const topBidResponse =
+    topBidRes as paths["/events/collections/top-bid/v1"]["get"]["responses"]["200"]["schema"];
 
-  if (!bidData) {
+  const topBid = topBidResponse.events?.[0];
+
+  if (!topBid?.event?.id || !topBid?.topBid?.price || !topBid?.topBid?.maker) {
     logger.error("Could not pull top bid");
     throw new Error("Could not pull top bid");
   }
 
   const initialId: string | null = await redis.get("bideventid");
-  if (bidData.event.id !== initialId) {
-    const success: "OK" = await redis.set("bideventid", bidData.topBid.price);
+  if (topBid.event.id !== Number(initialId)) {
+    const success: "OK" = await redis.set(
+      "bideventid",
+      topBid.event.id.toString()
+    );
     if (success !== "OK") {
       logger.error("Could not set new topbid eventid");
       throw new Error("Could not set new topbid eventid");
     }
-    const {
-      data: {
-        collections: [bidCollData],
-      },
-    } = await axios.get(
-      `https://api.reservoir.tools/collections/v5?id=${contractAddress}&includeTopBid=false&sortBy=allTimeVolume&limit=1`
-    );
+
+    const bidCollection = await sdk.getCollectionsV5({
+      id: contractAddress,
+      includeTopBid: "false",
+      sortBy: "allTimeVolume",
+      limit: "1",
+      accept: "*/*",
+    });
+
     const bidEmbed = new EmbedBuilder()
       .setColor(0x8b43e0)
       .setTitle("New Top Bid!")
       .setAuthor({
-        name: `${bidCollData.name}`,
+        name: `${bidCollection.name}`,
         url: "https://reservoir.tools/",
-        iconURL: `${bidCollData.image}`,
+        iconURL: `${bidCollection.image}`,
       })
       .setDescription(
-        `The top bid on the collection just went up to ${
-          bidData.topBid.price
-        }Ξ made by [${bidData[0].topBid.maker.substring(
+        `The top bid on the collection just changed to ${
+          topBid.topBid.price
+        }Ξ made by [${topBid.topBid.maker.substring(
           0,
           6
-        )}](https://www.reservoir.market/address/${bidData.topBid.maker})`
+        )}](https://www.reservoir.market/address/${topBid.topBid.maker})`
       )
-      .setThumbnail(`${bidCollData.image}`)
+      .setThumbnail(`${bidCollection.image}`)
       .setTimestamp();
     channel.send({ embeds: [bidEmbed] });
+    logger.info("Successfully alert new top bid");
   }
   setTimeout(bidPoll, 2500);
 }
--- floorPoll.ts
+++ floorPoll.ts
@@ -1,4 +1,3 @@
-import axios from "axios";
 import Redis from "ioredis";
 import {
   TextChannel,
@@ -7,50 +6,81 @@ import {
   ButtonBuilder,
 } from "discord.js";
 const redis = new Redis();
-// import { paths } from "@reservoir0x/reservoir-kit-client";
+import { paths } from "@reservoir0x/reservoir-kit-client";
 import logger from "./logger";
+const sdk = require("api")("@reservoirprotocol/v1.0#6e6s1kl9rh5zqg");
 
 export async function floorPoll(channel: TextChannel, contractAddress: string) {
   await redis.connect();
 
-  const {
-    data: {
-      events: [res],
-    },
-  } = await axios.get(
-    `https://api.reservoir.tools/events/collections/floor-ask/v1?collection=${contractAddress}&sortDirection=desc&limit=1`
-  );
-  /* const response =
-    res as paths["/events/collections/floor-ask/v1"]["get"]["responses"]["200"]["schema"]["events"][]; */
-  if (!res) {
+  const floorAskRes = await sdk.getEventsCollectionsFlooraskV1({
+    collection: contractAddress,
+    sortDirection: "desc",
+    limit: "1",
+    accept: "*/*",
+  });
+  const floorAskResponse =
+    floorAskRes as paths["/events/collections/floor-ask/v1"]["get"]["responses"]["200"]["schema"];
+
+  const floorAsk = floorAskResponse.events?.[0];
+
+  if (
+    !floorAsk?.event?.id ||
+    !floorAsk.collection?.id ||
+    !floorAsk.floorAsk?.tokenId ||
+    !floorAsk.floorAsk?.price
+  ) {
     logger.error("Could not pull floor ask");
     throw new Error("Could not pull floor ask");
   }
 
+  // pull initial floor ask event id
   const initialId: string | null = await redis.get("flooreventid");
-  if (res.event.id !== initialId) {
-    const success: "OK" = await redis.set("flooreventid", res.event.id);
+
+  if (floorAsk.event.id !== Number(initialId)) {
+    // setting new floor ask event id
+    const success: "OK" = await redis.set(
+      "flooreventid",
+      floorAsk.event.id.toString()
+    );
     if (success !== "OK") {
       logger.error("Could not set new floorprice eventid");
       throw new Error("Could not set new floorprice eventid");
     }
 
-    const {
-      data: {
-        tokens: [floorToken],
-      },
-    } = await axios.get(
-      `https://api.reservoir.tools/tokens/v5?tokens=${res.floorAsk.contract}%3A${res.floorAsk.tokenId}&sortBy=floorAskPrice&limit=1&includeTopBid=false&includeAttributes=true`
-    );
+    const tokenRes = await sdk.getTokensV5({
+      tokens: `${floorAsk.collection.id}:${floorAsk.floorAsk.tokenId}`,
+      sortBy: "floorAskPrice",
+      limit: "1",
+      includeTopBid: "false",
+      includeAttributes: "false",
+      accept: "*/*",
+    });
+    const tokenResponse =
+      tokenRes as paths["/tokens/v5"]["get"]["responses"]["200"]["schema"];
 
-    if (!floorToken) {
+    const floorToken = tokenResponse.tokens?.[0];
+
+    if (
+      !floorToken?.token?.collection ||
+      !floorToken?.token?.owner ||
+      !floorToken?.token?.lastSell ||
+      !floorToken?.token?.name
+    ) {
       logger.error("Could not pull floor token");
       throw new Error("Could not pull floor token");
     }
 
-    const attributes = floorToken.token.attributes.map((attr) => {
-      return { name: attr.key, value: attr.value, inline: true };
-    });
+    // create attributes array if they exist
+    let attributes:
+      | { name: string; value: string; inline: boolean }[]
+      | undefined = undefined;
+    if (floorToken.token.attributes) {
+      attributes = floorToken.token.attributes.map((attr) => {
+        return { name: attr.key ?? "", value: attr.value ?? "", inline: true };
+      });
+    }
+
     const floorEmbed = new EmbedBuilder()
       .setColor(0x8b43e0)
       .setTitle("New Floor Listing!")
@@ -61,7 +91,7 @@ export async function floorPoll(channel: TextChannel, contractAddress: string) {
       })
       .setDescription(
         `${floorToken.token.name} was just listed for ${
-          res.floorAsk.price
+          floorAsk.floorAsk.price
         }Ξ by [${floorToken.token.owner.substring(
           0,
           6
@@ -71,7 +101,7 @@ export async function floorPoll(channel: TextChannel, contractAddress: string) {
         
         `
       )
-      .addFields(attributes)
+      .addFields(attributes ?? [])
       .setThumbnail(`${floorToken.token.image}`)
       .setTimestamp();
 
@@ -80,11 +110,11 @@ export async function floorPoll(channel: TextChannel, contractAddress: string) {
         .setLabel("Purchase")
         .setStyle(5)
         .setURL(
-          `https://www.reservoir.market/${res.floorAsk.contract}/${res.floorAsk.tokenId}`
+          `https://www.reservoir.market/${floorAsk.collection.id}/${floorAsk.floorAsk.tokenId}`
         )
     );
     channel.send({ embeds: [floorEmbed], components: [row] });
+    logger.info("Successfully alerted new floor price");
   }
-
   setTimeout(floorPoll, 2500);
 }
